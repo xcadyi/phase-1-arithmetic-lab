e09f9c7b67db0f9550bc6ba5cc45dc8ca5533ee1
Selin Ayyildiz <xcadyix@gmail.com>
2022-04-06 10:49:32 -0400

    commit tree bf705d14681645b327764e5e607fca5749bb2f7e

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10)+ 1
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(4,8,12,16,20)
